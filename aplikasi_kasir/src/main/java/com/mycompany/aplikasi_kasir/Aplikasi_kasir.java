ca8dabbaec3cc2820b3d40a6f54cc07a50f56e44
reza choirudin divansya <rezadivansa@gmail.com>
2024-05-22 20:01:38 +0700

    Summry

---
+++ aplikasi_kasir/src/main/java/com/mycompany/aplikasi_kasir/Aplikasi_kasir.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasi_kasir;
+
+/**
+ *
+ * @author HP
+ */
+public class Aplikasi_kasir {
+
+    public static void main(String[] args) {
+      new form_utama().show();
+    }
+}
---
+++ aplikasi_kasir/src/main/java/com/mycompany/aplikasi_kasir/form_utama.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aplikasi_kasir;
+
+/**
+ *
+ * @author HP
+ */
+public class form_utama {
+
+    public form_utama() {
+    }
+
+    void show() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
